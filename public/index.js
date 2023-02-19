f3b3f9c5ae67785e120b35713ea621e128041193
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2023-02-19 16:52:29 +0530

    almost all done

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
 console.log("hello world!")
+function openNav(){
+    document.getElementById('menu-bar').style.display = "block";
+    document.getElementById('menu-bar').style.transition = "0.7s";
+    document.getElementById('menu-bar').style.transform = "translateY(0)";
+}
+
+// function closeNav(){
+//     document.getElementById('menu-bar').style.display = "none";
+//     document.getElementById('menu-bar').style.height = "0px";
+// }
