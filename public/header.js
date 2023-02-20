a9b1ff7e26c89bcf24e5a11adfdc39c917f80ed3
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2023-02-20 06:49:25 +0530

    Almost done

--- public/header.js
+++ public/header.js
@@ -1,12 +1,12 @@
 // header
 
 $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 0) {
-      $("header").addClass("active");
-    } else {
-      $("header").removeClass("active");
-    }
+  var scroll = $(window).scrollTop();
+  if (scroll > 0) {
+    $("header").addClass("active");
+  } else {
+    $("header").removeClass("active");
+  }
 });
-  
+
 // header
--- public/owl.js
+++ public/owl.js
@@ -1,15 +1,14 @@
 // owl
 $(document).ready(function() {
-    $("#news-slider").owlCarousel({
-        items : 5,
-        itemsDesktop:[1199,5],
-        itemsDesktopSmall:[980,2],
-        itemsMobile : [600,1],
-        navigation:true,
-        navigationText : ["",""],
-        pagination:true,
-        autoPlay:true
-    });
+  $("#news-slider").owlCarousel({
+      items : 5,
+      itemsDesktop:[1199,5],
+      itemsDesktopSmall:[980,2],
+      itemsMobile : [600,1],
+      navigation:true,
+      navigationText : ["",""],
+      pagination:true,
+      autoPlay:true
   });
+});
 // owl
-  
